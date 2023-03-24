7e658069f19e9c0b1484b5ff91a399ea71fe3ded
ivancicrok <ivancicrok@live.com>
2023-03-24 11:00:32 +0100

    adding shapes

--- src/Graphics/Screen.h
+++ src/Graphics/Screen.h
@@ -7,6 +7,9 @@
 
 class Vec2D;
 class Line2D;
+class Triangle;
+class AARectangle;
+class Circle;
 struct SDL_Window;
 struct SDL_Surface;
 
@@ -30,6 +33,9 @@ public:
 	* Bresenham's Line Algorithm is used to draw a line.
 	*/
 	void Draw(const Line2D& line, const Color& color);
+	void Draw(const Triangle& triangle, const Color& color);
+	void Draw(const AARectangle& rect, const Color& color);
+	void Draw(const Circle& circle, const Color& color);
 
 private:
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include "Color.h"
 #include "Screen.h"
 #include "Line2D.h"
+#include "Triangle.h"
+#include "AARectangle.h"
+#include "Circle.h"
 #include "Vec2D.h"
 #include "DynamicIntArray.h"
 #include "Utils.h"
@@ -20,56 +23,19 @@ int main(int argc, char* argv[])
     Screen theScreen;
     theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
 
-    Vec2D p1 = Vec2D(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-    Vec2D p2 = Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT);
-    Line2D line = { p1, p2 };
+    Line2D line = { Vec2D(0,0), Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT)};
+    Triangle triangle = { Vec2D(60, 10), Vec2D(10, 110), Vec2D(110, 110) };
+    AARectangle rect = { Vec2D(SCREEN_WIDTH / 2 - 25, SCREEN_HEIGHT / 2 - 25), 50, 50 };
+    Circle circle = { Vec2D(SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2 + 50), 50 };
 
-    theScreen.Draw(line, Color::White());
+    theScreen.Draw(triangle, Color::Red());
+    theScreen.Draw(rect, Color::Blue());
+    theScreen.Draw(circle, Color::Pink());
+    //theScreen.Draw(line, Color::White());
 
     //theScreen.Draw(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, Color::Yellow());
     theScreen.SwapScreen();
 
-
-
-    // Pentagon
-    //int size = 20;
-    //Vec2D center = Vec2D(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-    //float radRotate = DegToRad(72);
-
-    //Vec2D pp1 = Vec2D(center.GetX(), center.GetY() - size);
-    //Vec2D pp2 = pp1.RotationResult(radRotate, center);
-    //Vec2D pp3 = pp2.RotationResult(radRotate, center);
-    //Vec2D pp4 = pp3.RotationResult(radRotate, center);
-    //Vec2D pp5 = pp4.RotationResult(radRotate, center);
-
-    //theScreen.Draw(pp1, Color::Yellow());
-    //theScreen.Draw(pp2, Color::Yellow());
-    //theScreen.Draw(pp3, Color::Yellow());
-    //theScreen.Draw(pp4, Color::Yellow());
-    //theScreen.Draw(pp5, Color::Yellow());
-
-    //Line2D line1 = Line2D(pp1, pp3);
-    //Line2D line2 = Line2D(pp3, pp5);
-    //Line2D line3 = Line2D(pp5, pp2);
-    //Line2D line4 = Line2D(pp2, pp4);
-    //Line2D line5 = Line2D(pp4, pp1);
-
-    //theScreen.Draw(line1, Color::Yellow());
-    //theScreen.Draw(line2, Color::Yellow());
-    //theScreen.Draw(line3, Color::Yellow());
-    //theScreen.Draw(line4, Color::Yellow());
-    //theScreen.Draw(line5, Color::Yellow());
-
-    Vec2D starPosition = Vec2D(SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2);
-    Star star(starPosition, 20);
-    std::vector<Line2D> starLines = star.GetLines();
-
-    for (int i = 0; i < starLines.size(); i++)
-    {
-        theScreen.Draw(starLines.at(i), Color::Blue());
-    }
-
-    theScreen.SwapScreen();
 
     SDL_Event sdlEvent;
     bool running = true;
@@ -85,23 +51,6 @@ int main(int argc, char* argv[])
                 break;
             }
         }
-
-        // Rotating line
-        /*line.SetP1(line.GetP1().RotationResult(0.001, p1));
-        theScreen.Draw(line, Color::White());*/
-
-        // Rotating line
-        line.SetP1(line.GetP1().RotationResult(0.005, p1));
-        theScreen.Draw(line, Color::White());
-
-        // Draw star
-        for (int i = 0; i < star.GetLines().size(); i++)
-        {
-            theScreen.Draw(star.GetLines().at(i), Color::Blue());
-        }
-        star.Rotate(0.005, p1);
-
-        theScreen.SwapScreen();
     }
 
     return 0;
